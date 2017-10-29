ce1e2480e002d9c3b93a27791304255681b05d32
ebenner91 <ebenner@mail.greenriver.edu>
2017-10-28 19:43:17 -0700

    Created click listener in MediaViewHolder and Intent getter in MediaItemDetailActivity

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/MediaItemDetailActivity.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/MediaItemDetailActivity.java
@@ -3,7 +3,9 @@ package co.miniforge.corey.mediatracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import co.miniforge.corey.mediatracker.model.MediaItem;
@@ -15,9 +17,29 @@ import co.miniforge.corey.mediatracker.model.MediaItem;
  * the code in MyListActivity)
  */
 public class MediaItemDetailActivity extends AppCompatActivity {
+
+    MediaItem item;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_item_detail);
+
+        String extraData;
+        if(getIntent().hasExtra("extraTag")){
+            extraData = getIntent().getStringExtra("extraTag");
+            try {
+                JSONObject mediaJson = new JSONObject(extraData);
+                item = new MediaItem(mediaJson);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
     }
+
 }
--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/media_recycler/MediaViewHolder.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/media_recycler/MediaViewHolder.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import co.miniforge.corey.mediatracker.MediaItemDetailActivity;
 import co.miniforge.corey.mediatracker.MyListActivity;
@@ -45,8 +46,10 @@ public class MediaViewHolder extends RecyclerView.ViewHolder {
         inflated.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: Create a new activity with this object's data
-                //Hint: mediaItem.toJson().toString() && context.startActivity);
+                Intent intent = new Intent(context, MediaItemDetailActivity.class);
+                intent.putExtra("extraTag", mediaItem.toJson().toString());
+                context.startActivity(intent);
+
             }
         });
     }
