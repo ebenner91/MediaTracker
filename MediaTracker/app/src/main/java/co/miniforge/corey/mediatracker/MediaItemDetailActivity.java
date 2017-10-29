7b0a02a2b205d3512af274a022cdb9639d3879f3
ebenner91 <ebenner@mail.greenriver.edu>
2017-10-28 20:53:17 -0700

    Added functionality to fill EditText elements with data taken from the intent, then when save button is clicked, take data from the EditTexts and send it back to MyListActivity with an intent

--- MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/MediaItemDetailActivity.java
+++ MediaTracker/app/src/main/java/co/miniforge/corey/mediatracker/MediaItemDetailActivity.java
@@ -3,7 +3,9 @@ package co.miniforge.corey.mediatracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,10 +17,17 @@ import co.miniforge.corey.mediatracker.model.MediaItem;
  * of the item. When the user clicks the save button, the activity should create an intent that goes
  * back to MyListActivity and puts the MediaItem into the intent (If you are stuck on that, read through
  * the code in MyListActivity)
+ *
+ * @author Elizabeth Benner
+ * @version 1.0
  */
 public class MediaItemDetailActivity extends AppCompatActivity {
 
     MediaItem item;
+    EditText title;
+    EditText description;
+    EditText url;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +45,47 @@ public class MediaItemDetailActivity extends AppCompatActivity {
             }
         }
 
+        getEditTexts();
+        getButton();
+        fillEdits();
+        saveButtonListener();
 
+    }
 
+    void getEditTexts() {
+        title = (EditText) findViewById(R.id.mediaTitle);
+        description = (EditText) findViewById(R.id.mediaDescription);
+        url = (EditText) findViewById(R.id.mediaURL);
+    }
 
+    void getButton() {
+        saveButton = (Button) findViewById(R.id.saveButton);
+    }
 
+    void fillEdits() {
+        title.setText(item.title);
+        description.setText(item.description);
+        url.setText(item.url);
+    }
+
+    void saveButtonListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTitle = title.getText().toString();
+                String editDescription = description.getText().toString();
+                String editURL = url.getText().toString();
+
+                item.title = editTitle;
+                item.description = editDescription;
+                item.url = editURL;
+
+                Intent intent = new Intent(getApplicationContext(), MyListActivity.class);
+                intent.putExtra("mediaExtra", item.toJson().toString());
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
